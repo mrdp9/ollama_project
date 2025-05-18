3ccb7fabc7765d19f98f96a91685b5c777ec4b53
Mridul Pandey <mridulpandey999@gmail.com>
2025-05-19 00:09:53 +0530

    final commit for locally running ollama model with docker based frontend

--- dependencies.sh
+++ dependencies.sh
@@ -2,5 +2,5 @@
 sudo apt update 
 sudo apt install curl -y 
 docker build -t frontend2 .
-docker run -p 80:80 frontend2 && ollama run llama3.2:3b
 ./ollama.sh
+docker run -p 80:80 frontend2 && ollama run llama3.2:3b
